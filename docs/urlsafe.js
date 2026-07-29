b40cb6177303978720d2366097fca5470371935a
Noah Kiser <contact@noahkiser.com>
2026-07-29 17:59:23 -0500

    add lempel-ziv and url-safe base64 encoding to file outputs (now .frs instead of .json)

---
+++ docs/urlsafe.js
@@ -0,0 +1,1 @@
+var E={'+':'-','/':'_','=':'.'};var D={'-':'+',_: '/','.':'='};function encode(b){return b.replace(/[+/=]/g,function(m){return E[m];});};function decode(s){return s.replace(/[-_.]/g,function(m){return D[m];});};
